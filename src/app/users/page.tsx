a1318d906838b5b134e543a8d7fadbd96905aa82
Edson Mallet <edsonmmr@gmail.com>
2023-12-13 16:56:06 -0300

    feat: add eslint plugins

---
+++ src/app/users/page.tsx
@@ -0,0 +1,12 @@
+import { api } from "@/trpc/server";
+
+export default async function Home() {
+  const { greeting } = await api.product.get.query({ text: "Users" });
+  return (
+    <div className="container pt-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        {greeting}
+      </div>
+    </div>
+  );
+}
